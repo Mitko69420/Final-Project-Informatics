2fef4095e071f77714469e4251d6ec5b53afb149
dimit <d.borilov26@acsbg.org>
2025-06-08 19:00:53 +0300

    Final

--- src/Persistence/BinaryStorage.java
+++ src/Persistence/BinaryStorage.java
@@ -21,25 +21,13 @@ public class BinaryStorage {
     //Load from bin file
     public static List<HardwareComponent> loadFromFile(String filename) {
         File file = new File(filename);
-        if (!file.exists()) {
-            return new ArrayList<>();
-        }
+        if (!file.exists()) return new ArrayList<>();
 
         try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(file))) {
-            Object obj = in.readObject();
-            if (obj instanceof List<?>) {
-                List<HardwareComponent> loaded = new ArrayList<>();
-                for (Object o : (List<?>) obj) {
-                    if (o instanceof HardwareComponent) {
-                        loaded.add((HardwareComponent) o);
-                    }
-                }
-                System.out.println("Loaded from binary file: " + filename);
-                return loaded;
-            }
-        } catch (IOException | ClassNotFoundException e) {
+            return (List<HardwareComponent>) in.readObject();
+        } catch (Exception e) {
             System.out.println("Error loading file: " + e.getMessage());
+            return new ArrayList<>();
         }
-        return new ArrayList<>();
     }
 }
